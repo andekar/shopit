06e79d2013ea1a4fc63196928964ff20ca414acd
Anders <andersk84@gmail.com>
2018-08-24 14:42:27 +0200

    add webmachine

--- apps/shopit/src/shopit_sup.erl
+++ apps/shopit/src/shopit_sup.erl
@@ -1,38 +1,20 @@
-%%%-------------------------------------------------------------------
-%% @doc shopit top level supervisor.
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(shopit_sup).
-
 -behaviour(supervisor).
 
-%% API
--export([start_link/0]).
+%% External exports
+-export([
+  start_link/0
+]).
 
-%% Supervisor callbacks
+%% supervisor callbacks
 -export([init/1]).
 
--define(SERVER, ?MODULE).
-
-%%====================================================================
-%% API functions
-%%====================================================================
-
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%%====================================================================
-%% Supervisor callbacks
-%%====================================================================
-
-%% Child :: #{id => Id, start => {M, F, A}}
-%% Optional keys are restart, shutdown, type, modules.
-%% Before OTP 18 tuples must be used to specify a child. e.g.
-%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
+    Web = {webmachine_mochiweb,
+           {webmachine_mochiweb, start, [shopit_config:web_config()]},
+           permanent, 5000, worker, [mochiweb_socket_server]},
+    Processes = [Web],
+    {ok, { {one_for_one, 10, 10}, Processes} }.
